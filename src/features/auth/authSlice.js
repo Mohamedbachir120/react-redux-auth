59fa202ef72a0d141bc0b12648f7fb3d54406748
Mohamedbachir120 <mohamedbhadjadji@gmail.com>
2022-11-07 07:44:17 +0100

    first commit

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,22 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const authSlice  = createSlice({
+    name:'auth',
+    initialState:{user:null,token:null},
+    reducers:{
+        setCredentials:(state,action)=>{
+            const {user,accessToken} = action.payload
+            state.user = user
+            state.token = accessToken
+
+        },
+        logOut:(state,action)=>{
+            state.user = null
+            state.token = null
+        }
+    }
+})
+export const {setCredentials,logOut} = authSlice.actions
+export default authSlice.reducer
+export const selectCurentUser = (state) => state.auth.user
+export const selectCurentToken = (state) => state.auth.token
